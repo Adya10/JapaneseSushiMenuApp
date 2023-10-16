cce81b0176811dcdc891fcce67d8f1e51fab0650
Advait Lanjekar <adya.lanjekar@gmail.com>
2023-10-16 12:38:23 +0200

    Initial Commit

---
+++ menuApp/menuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  menuAppApp.swift
+//  menuApp
+//
+//  Created by Advait Lanjekar on 16/10/23.
+//
+
+import SwiftUI
+
+@main
+struct menuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
